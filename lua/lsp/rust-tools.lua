53624c9f83c241c16b077028f5ac1e8592890631
nick@p50 <synico@qq.com>
2023-03-16 23:53:30 +0800

    feat: enable code navigation for rust
    Signed-off-by: nick@p50 <synico@qq.com>

--- lua/lsp/rust-tools.lua
+++ lua/lsp/rust-tools.lua
@@ -11,6 +11,17 @@ vim.opt.shortmess = vim.opt.shortmess + "c"
 local function on_attach(client, buffer)
   -- This callback is called when the LSP is atttached/enabled for this buffer
   -- we could set keymaps related to LSP, etc here.
+  local keymap_opts = { buffer = buffer }
+  -- Code navigation and shortcuts
+  vim.keymap.set("n", "<c-]>", vim.lsp.buf.definition, keymap_opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, keymap_opts)
+  vim.keymap.set("n", "gD", vim.lsp.buf.implementation, keymap_opts)
+  vim.keymap.set("n", "<c-k>", vim.lsp.buf.signature_help, keymap_opts)
+  vim.keymap.set("n", "1gD", vim.lsp.buf.type_definition, keymap_opts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, keymap_opts)
+  vim.keymap.set("n", "g0", vim.lsp.buf.document_symbol, keymap_opts)
+  vim.keymap.set("n", "gW", vim.lsp.buf.workspace_symbol, keymap_opts)
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opts)
 end
 
 -- Configure LSP through rust-tools.nvim plugin.
@@ -49,6 +60,7 @@ local opts = {
 }
 
 require("rust-tools").setup(opts)
+
 -- Setup Completion
 -- See https://github.com/hrsh7th/nvim-cmp#basic-configuration
 local cmp = require("cmp")
