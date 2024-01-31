6ee5bc0f6c1d32c20a3f74bc20e4c440437cb646
Nick <synico@gmail.com>
2024-01-31 14:55:17 +0800

    feat: add configuration for bufferline
    Signed-off-by: Nick <synico@gmail.com>

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -7,14 +7,31 @@ end
 
 bufferline.setup({
   options = {
-    close_command = "Bdelete! %d",
+    mode = "buffers", -- set to "tabs" to only show tabpages instead
+    --style_preset = bufferline.style_preset.default, -- or bufferline.style_preset.minimal,
+    themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
+    numbers = "buffer_id",
+    close_command = "bdelete! %d",       -- can be a string | function, | false see "Mouse actions"
+    right_mouse_command = "bdelete! %d", -- can be a string | function | false, see "Mouse actions"
+    left_mouse_command = "buffer %d",    -- can be a string | function, | false see "Mouse actions"
+    middle_mouse_command = nil,          -- can be a string | function, | false see "Mouse actions"
+    indicator = {
+      icon = '▎', -- this should be omitted if indicator style is not 'icon'
+      style = 'underline',
+    },
+    buffer_close_icon = '',
+    modified_icon = '●',
+    close_icon = '',
+    left_trunc_marker = '',
+    right_trunc_marker = '',
     offsets = {
       {
         filetype = "NvimTree",
-	text = "File Explorer",
-	highlight = "Directory",
-	text_align = "left",
-      },
+        text = "File Explorer",
+        highlight = "Directory",
+        text_align = "left",
+      }
     },
-  },
+    color_icons = true 
+  }
 })
